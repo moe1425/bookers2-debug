aa01f37251300acbd99810f8c9ca13f28aaab1d8
EC2 Default User <ec2-user@ip-172-31-38-29.ap-northeast-1.compute.internal>
2022-07-25 12:28:10 +0000

    Finish

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -6,23 +6,22 @@ class ChatsController < ApplicationController
     rooms = current_user.user_rooms.pluck(:room_id)
     user_rooms = UserRoom.find_by(user_id: @user.id, room_id: rooms)
 
-    if user_rooms.nil?
+    unless user_rooms.nil?
+      @room = user_rooms.room
+    else
       @room = Room.new
       @room.save
-      UserRoom.create(user_id: @user.id, room_id: @room.id)
       UserRoom.create(user_id: current_user.id, room_id: @room.id)
-    else
-      @room = user_rooms.room
+      UserRoom.create(user_id: @user.id, room_id: @room.id)
     end
 
-    #roomに紐づくchatsテーブルのレコードを@chatsに格納
     @chats = @room.chats
     @chat = Chat.new(room_id: @room.id)
   end
 
   def create
     @chat = current_user.chats.new(chat_params)
-    @chat.save
+    render :validater unless @chat.save
   end
 
   private
